a6be30a66a485eebed00f36c249277e1b6679eeb
yonahara-yu <yu.yonahara@gmail.com>
2026-02-09 01:01:11 +0900

    fix: page text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1>与那原悠のウェブアプリ</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
